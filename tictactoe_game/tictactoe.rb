156dd3c487b57c0eb9bdf86ac666f0d9537cef8c
Jack Sebben <jacksebben11@hotmail.com>
2024-05-22 22:30:34 -0400

    Game successfully initializes a new TTTGame object along with Player objects and a Board object as collaborators to the TTTGame. The Board object successfully initializes a 3x3 Array Matrix populated with Square objects as collaborators.

--- tictactoe_game/tictactoe.rb
+++ tictactoe_game/tictactoe.rb
@@ -1,10 +1,15 @@
 ## TTT Game
 
 class TTTGame
+  attr_reader :board, :player1, :player2
 
+  include GameDisplays
+  
   def initialize
-    @board = Board.new
+    @board   = Board.new
+
     @player1 = Player.new
+    @player2 = Player.new(@player1)
   end
 
   def play
@@ -14,10 +19,12 @@ class TTTGame
       puts board
       break if board.is_full?
       
-      player1.take_turn
+      player1_move = player1.place_marker
+      board.update(player1_move)
       break if board.winning_line?
       
-      player2.take_turn
+      player2_move = player2.place_marker
+      board.update(player2_move)
       break if board.winning_line?
     end
 
@@ -25,3 +32,82 @@ class TTTGame
     goodbye
   end
 end
+
+class Player
+  attr_reader :name, :marker
+
+  def initialize(other_player=nil)
+    @name   = Player.choose_name(other_player)
+    @marker = Player.choose_marker(other_player)
+  end
+
+  def self.choose_name(other_player)
+    case other_player
+    when nil then puts "Player 1, what's your name?"
+    else          puts "Player 2, what's your name?"
+    end
+
+    name = ''
+
+    loop do # Get Player's name
+      name = gets.chomp
+
+      if other_player && name.capitalize == other_player.name
+        puts "Sorry, the name '#{name}' was taken by the other player."
+        next
+      end 
+      
+      unless name.size > 2 && name.match?(/[0-9a-z]/i) 
+        puts "Stop messing around and enter a valid name..."
+        puts "A valid name being: A name with at least 2 characters and includes letters and/or numbers in it"
+        next
+      end
+
+      break
+    end
+
+    name.capitalize
+  end
+
+  def self.choose_marker(other_player)
+    case other_player
+    when nil then puts "Player 1, what's your marker?"
+    else          puts "Player 2, what's your marker?"
+    end
+
+    marker = ''
+
+    loop do # Get Player's marker
+      marker = gets.chomp
+
+      if other_player && marker.upcase == other_player.marker
+        puts "Sorry, the marker '#{marker}' was taken by the #{other_player.name}."
+        next
+      end 
+      
+      unless marker.match?(/[a-z]/i) && marker.size == 1
+        puts "Marker must be a single letter. Please try again."
+        next
+      end
+
+      break
+    end
+
+    marker.upcase
+  end
+end
+
+class Board
+
+  def initialize
+    @squares = Array.new(3) { Array.new(3) { Square.new } } 
+  end
+
+
+end
+
+class Square
+  attr_accessor :mark
+end
+
+game = TTTGame.new
